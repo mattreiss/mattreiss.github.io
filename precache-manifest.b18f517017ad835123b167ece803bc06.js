60fc08c171218af435c117e8ac1048b20f2de96a
matt <matt@Matts-MacBook-Pro.local>
2019-04-17 12:05:43 -0700

    Updates

---
+++ precache-manifest.b18f517017ad835123b167ece803bc06.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9f4da056a1dea3dcd13",
+    "url": "/static/css/main.241cf670.chunk.css"
+  },
+  {
+    "revision": "e9f4da056a1dea3dcd13",
+    "url": "/static/js/main.831d2152.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f398a0e9afd0203b46f1",
+    "url": "/static/js/2.3125dc2b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c7ced7cf0b7c35edc0f3e40d44aa8cc2",
+    "url": "/static/media/bay-bridge.c7ced7cf.mp4"
+  },
+  {
+    "revision": "d6b1f61dcba6f39f0d129d21eadf2008",
+    "url": "/static/media/panther-beach.d6b1f61d.m4v"
+  },
+  {
+    "revision": "bf45f93181925d8a791a08648231b431",
+    "url": "/static/media/mcway-falls.bf45f931.mp4"
+  },
+  {
+    "revision": "e2b000f2352b0e1ba0d0e35f03bcb746",
+    "url": "/static/media/sunset.e2b000f2.m4v"
+  },
+  {
+    "revision": "6cda64d7d41d9c0e55a85284a8a6969c",
+    "url": "/static/media/turtle.6cda64d7.mp4"
+  },
+  {
+    "revision": "a68abc37b78bca1fb0eb0c1ea65d1dd2",
+    "url": "/static/media/badwater-basin.a68abc37.m4v"
+  },
+  {
+    "revision": "5e80123baeee7fa6f9f055bb01be9c57",
+    "url": "/static/media/dunes.5e80123b.mp4"
+  },
+  {
+    "revision": "c6dc30b568b7db4b0333cd7b78662d07",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.844a23f03805a68ade239604ca63e7ce.js"
+  "/precache-manifest.b18f517017ad835123b167ece803bc06.js"
 );
 
 workbox.clientsClaim();
