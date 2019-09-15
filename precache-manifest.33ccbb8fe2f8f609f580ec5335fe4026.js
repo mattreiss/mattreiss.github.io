7753a55dac786684de164279760df61c5a895519
m <m@ms-Mac-mini.hsd1.ca.comcast.net>
2019-09-14 23:23:10 -0700

    Updates

---
+++ precache-manifest.33ccbb8fe2f8f609f580ec5335fe4026.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819ba5cd4c2e1d5aac80cb6dd095918e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5eb5b78ff97972fd917",
+    "url": "/static/css/main.6e0b7b2d.chunk.css"
+  },
+  {
+    "revision": "48a8cee2234994349046",
+    "url": "/static/js/2.1679ed36.chunk.js"
+  },
+  {
+    "revision": "b5eb5b78ff97972fd917",
+    "url": "/static/js/main.c9f612c0.chunk.js"
+  },
+  {
+    "revision": "416cc1c79b8c22940a8c",
+    "url": "/static/js/runtime~main.7302273d.js"
+  }
+]);
