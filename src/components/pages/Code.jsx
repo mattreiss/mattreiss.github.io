8051d64a550afb1daf086e2504fed815f0935197
m <m@ms-Mac-mini.local>
2019-09-17 23:38:54 -0700

    Clean Deploy 2

--- src/components/pages/Code.jsx
+++ src/components/pages/Code.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {
   CodeSnippet,
 } from '../elements';
@@ -19,11 +20,61 @@ class Code extends React.Component {
     return (
       <Section>
         <Article>
+          <Title>Responsive Images with Styled Components</Title>
+          <SubTitle>09/17/2019</SubTitle>
+          <Paragraph>
+            Here is a cool hack I did to display responsive images in a grid with styled components.
+          </Paragraph>
+          <CodeSnippet
+            url='https://raw.githubusercontent.com/mattreiss/mattreiss.github.io/clean/src/components/pages/Photos.jsx'
+            start={146}
+            end={150}
+            showLines={true}
+          />
+          <Paragraph>
+            I first created an Image component that takes in props called 'scale' and 'isSquare',
+            which determine the width of the image (e.g scale=1 fills the screen and scale=2 is half of the screen)
+            and if the image should be rendered to fill a square parent (cropping and centering if necessary).
+            Here is the code that updates the dimensions based on those props and the image's original aspect ratio.
+          </Paragraph>
+          <CodeSnippet
+            url='https://raw.githubusercontent.com/mattreiss/mattreiss.github.io/clean/src/components/elements/Image.jsx'
+            start={39}
+            end={66}
+            showLines={true}
+          />
+          <Paragraph>
+            Then I created multiple styled components that wrap the Image component with the 'scale' prop set accordingly and use media queries to determine when to display the component.
+          </Paragraph>
+          <CodeSnippet
+            url='https://raw.githubusercontent.com/mattreiss/mattreiss.github.io/clean/src/components/pages/Photos.jsx'
+            start={38}
+            end={83}
+            showLines={true}
+          />
+          <Paragraph>
+            Finally I combined all of those components into a single component called ResponsiveImage.
+          </Paragraph>
+          <CodeSnippet
+            url='https://raw.githubusercontent.com/mattreiss/mattreiss.github.io/clean/src/components/pages/Photos.jsx'
+            start={90}
+            end={100}
+            showLines={true}
+          />
+          <Paragraph>
+            I could probably improve on how this is done, since it renders each image for every single screen size and just hides and shows them, but for now it seems to work pretty good!
+          </Paragraph>
+          <Paragraph>
+            Check out the <Link to="/Photos">Photos</Link> section to see the responsive images change as you change your screen size!
+          </Paragraph>
+        </Article>
+
+        <Article>
           <Title>Chess</Title>
           <SubTitle>09/14/2019</SubTitle>
           <Paragraph>
             I am working on a Chess App!
-            I want to make Anti-Chess.
+            Here is some code...
           </Paragraph>
           <CodeSnippet
             url='https://raw.githubusercontent.com/mattreiss/chess/chess/src/data/models/PieceModel.js'
@@ -34,8 +85,6 @@ class Code extends React.Component {
           <Paragraph>
             Those are how the Pieces are valued.
             Looks neat huh?
-            It's gonna have realtime multiplayer.
-            I am using Firebase Firestore.
           </Paragraph>
           <CodeSnippet
             url='https://raw.githubusercontent.com/mattreiss/chess/chess/src/data/models/PieceModel.js'
@@ -46,7 +95,6 @@ class Code extends React.Component {
           <Paragraph>
             These are some other functions.
             They do stuff.
-            Thanks for Reading!
           </Paragraph>
         </Article>
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -21,18 +21,14 @@ class Home extends React.Component {
       <Section>
         <Article>
           <Title>About Me</Title>
-          <SubTitle>09/15/2019</SubTitle>
+          <SubTitle>09/17/2019</SubTitle>
           <Paragraph>
             Skateboarding, Photography, and Coding have become my favorite outlets for me to express myself.
-            Falling over and over again until I finally stick a landing is also my favorite way of exercising.
-            Photography also requires some excersize, like hiking, but for me it is a little more relaxing.
-            Being out in nature and enjoying a sunset or the milkyway passing by while my camera shoots a timelapse is how I like to meditate.
-            And then when I finish editing, posting, and sometimes printing a photo, I have something I can look back at and get the same feeling I had when I was there.
-            I like to write Photoshop scripts to automate my timelapse processing and create different effects.
-            When I am not going out to skate or shoot photos, because I get injured a lot, I work on software projects.
-            I am most interested in working on projects that combine things that I am passionate about.
-            I have the problem of having too many app ideas and starting new projects that I end up not finishing.
-            This website will probably be another one of those projects, but hopefully not!
+            Falling over and over again until I finally land a trick on my skateboard is painful and fun.
+            Capturing a moment in time with my camera is sick too.
+            I also try to work on software projects that combine things that I am passionate about.
+            But I have the problem of having too many app ideas and starting new projects that I end up not finishing.
+            This website will probably be another one of those projects.
           </Paragraph>
         </Article>
       </Section>
